e1aef68633ad8f25558df6e6b93f3208049fb05f
tejal028 <tejalgohil028@gmail.com>
2025-06-02 23:49:38 +0530

    Modified my server.js file to connct with DocumentDB on AWS

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/bookstore', {
+// mongoose.connect('mongodb://localhost:27017/bookstore', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => console.log('MongoDB connected'))
+//   .catch(err => console.log(err));
+
+  mongoose.connect('mongodb://mycartuser:mycartpassword123@mycart-docdb-cluster-0.cl8u48saqn8m.ap-south-1.docdb.amazonaws.com:27017/bookstore?ssl=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
+  sslValidate: false, // Important: DocumentDB uses self-signed cert, skip validation
+}).then(() => console.log('Connected to AWS DocumentDB'))
+  .catch(err => console.error('DocumentDB connection error:', err));
+  
 app.use('/api/books', bookRoutes);
 app.use(express.static(path.join(__dirname, 'public'))); // serve index.html
 
